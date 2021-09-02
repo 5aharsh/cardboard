e14900c2f4b1959842f2fd640dcae18841343472
Saharsh Anand <saharshpro@hotmail.com>
2021-09-02 17:32:05 +0530

    Added config panel

--- assets/scripts/configure.js
+++ assets/scripts/configure.js
@@ -1,11 +1,41 @@
 const ls = localStorage
+
+
 var false_anchors = document.querySelectorAll(".false-anchor")      // false click anchor
-var username = document.getElementById("greet-name")                // greeting name input field
-var darkmode = document.getElementById("dark-mode")
+
+const anchorHandle = (e)=>{
+    e.preventDefault()
+}
 
 false_anchors.forEach((anchor)=>{
-    anchor.onclick = (e)=>{e.preventDefault()}
+    anchor.addEventListener('click', anchorHandle, false)
 })
 
+var config_btn = document.getElementById("config-btn")
+var config_box = document.getElementById("configure")
+var main_display = document.getElementById("display-box")
+var username = document.getElementById("greet-name")                // greeting name input field
+var darkmode = document.getElementById("dark-mode")
+var save_config = document.getElementById("save-config")
+var close_config = document.getElementById("close-config")
+
+// load data from localstorage
 username.value = ls.getItem("username")
-darkmode.checked = ls.getItem("darkmode")
+darkmode.checked = JSON.parse(ls.getItem("darkmode").toLowerCase())
+
+//save configs
+save_config.onclick = (e)=>{
+    ls.setItem("username", username.value)
+    ls.setItem("darkmode", darkmode.checked)
+    console.log(username.value, darkmode.checked);
+}
+
+close_config.onclick = (e)=>{
+    config_box.style.display = "none"
+    main_display.style.display = "grid"
+}
+
+config_btn.onclick = (e)=>{
+    config_box.style.display = "grid"
+    main_display.style.display = "none"
+}
